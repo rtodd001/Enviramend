ddbfbf68123500eb7bec4e010e2db71e48682246
Siena Ha <sha021@ucr.edu>
2019-04-27 05:33:05 -0700

    UI button java file

--- app/src/main/java/Citrus/enviramend/MainActivity.java
+++ app/src/main/java/Citrus/enviramend/MainActivity.java
@@ -1,13 +1,38 @@
-package Citrus.enviramend;
+package com.example.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //creates button scanBtn, use findVewById to get a result and we cast it by (Button)
+        Button scanBtn = (Button) findViewById(R.id.scanBtn);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
