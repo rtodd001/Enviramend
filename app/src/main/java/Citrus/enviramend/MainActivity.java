ae2c0595db2e8030c0a50adfc9c4ffbc11e4797e
rjtodd <ronan.jtodd@gmail.com>
2019-04-28 01:19:49 -0700

    Barcode added

--- app/src/main/java/Citrus/enviramend/MainActivity.java
+++ app/src/main/java/Citrus/enviramend/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         scanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, BarcodeScan.class);
+                Intent intent = new Intent(MainActivity.this, VisionActivity.class);
+                //Intent intent = new Intent(MainActivity.this, BarcodeScan.class);
                 startActivityForResult(intent,INTENT_CODE);
             }
         });
